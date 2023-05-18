d90f0713101a8c16cc081363d3e46638dc9fe9f0
John Klink <john.r.klink92@gmail.com>
2023-05-17 19:51:44 -0500

    worked on db
    many changes made to start database

---
+++ db/scripts/seedDb.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Pet = require('../models/pet.js');
+
+const petSeeds = require('../seeds/petsSeed.json');
+
+require('dotenv').config();
+
+const {USER, PASSWORD} = process.env;
+
+mongoose
+  .connect(`mongodb://${USER}:${PASSWORD}@localhost:27017/mydatabase`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const seedPets = async () => {
+  await Pet.deleteMany({});
+  await Pet.insertMany(petSeeds);
+};
+
+seedPets()
+.then(() => {
+  mongoose.connection.close();
+})
+.catch((error) => {
+  console.error('Failed to Seed Pet Data to Database', error);
+  mongoose.connection.close();
+});
