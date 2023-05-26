51c49ced89134fc3c6e031c927d817e948145257
John Klink <john.r.klink92@gmail.com>
2023-05-25 22:14:56 -0500

    deployment

--- server/server.js
+++ server/server.js
@@ -19,16 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-// Serve up static assets
-// app.use('/images', express.static(path.join(__dirname, '../client/images')));
+Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 //create a new instance of ApolloServer with the graphQL schema
 const startApolloServer = async(typeDefs, resolvers) => {
